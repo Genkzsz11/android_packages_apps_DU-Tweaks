610271772dae29ed2f0cb1bbcfa601d285c34a4a
Genkzsz11 <Genkzsz11@gmail.com>
2020-06-07 16:44:19 +0700

    k

--- src/com/dirtyunicorns/tweaks/preferences/CategoryPreference.java
+++ src/com/dirtyunicorns/tweaks/preferences/CategoryPreference.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2015 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.dirtyunicorns.tweaks.preferences;
 
 import android.content.Context;
@@ -22,31 +6,37 @@ import android.util.AttributeSet;
 import android.view.View;
 import androidx.core.content.res.TypedArrayUtils;
 import androidx.preference.Preference;
-import androidx.cardview.widget.CardView;
-import android.widget.GridLayout;
-import android.widget.GridView;
+import androidx.preference.PreferenceViewHolder;
+
 import com.android.settings.R;
 import com.android.settings.Utils;
 
-public class CategoryPreference extends Preference implements View.OnClickListener {
+public class CategoryPreference extends Preference {
 
     private final View.OnClickListener mClickListener = v -> performClick(v);
 
-    GridLayout gridLayout;
-    CardView cardView;
+    private boolean mAllowDividerAbove;
+    private boolean mAllowDividerBelow;
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public CategoryPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
 
-        View view = inflater.inflate(R.layout.category_preference, container, false);
-        gridLayout = (GridLayout) view.findViewById (R.id.main_grid);
-        cardView = (CardView) view.findViewById(R.id.perfCard);
-        return view;
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.Preference);
+        a.recycle();
+
+        setLayoutResource(R.layout.category_preference);
     }
 
-    cardview.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-        v.itemView.setOnClickListener(mClickListener);
-        }
-    });
- } 
+    public CategoryPreference(Context context, View view) {
+        super(context);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        holder.itemView.setOnClickListener(mClickListener);
+        final boolean selectable = isSelectable();
+        holder.itemView.setFocusable(selectable);
+        holder.itemView.setClickable(selectable);
+    }
+}
